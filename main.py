72d2d6bde78a91c192d59e3d6bce80699139e9b9
Glu(k)hov Kirill <kirill.gluhov2003@yandex.ru>
2018-12-31 18:54:28 +0300

    Добавлено несколько функций

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from discord.ext import commands
 import sys
 
 
-owner = "Kirill"
+owner = "Kirill#8322"
+operators = [owner, "Andrew 45743#5197"]
 TOKEN = 'NTI3ODMzNjMxMjM5MTc2MTk2.DwZlkQ.1i3psy23Ot75lh4maHE2C2ZWDpk'
-prefix = '$'
+prefix = '!'
 prior_channel = "main"
-greeting = "Всем привет! :wave:"
+greeting = "Всем привет! :wave: \n (P.S. Префикс для команд: \"{}\")".format(prefix)
+log_output = "output.txt"
 
 client = commands.Bot(command_prefix=prefix, command_not_found="Я не знаю такой команды (\""+prefix+"{}\")")
 
-def check_acces(roles, name=False):
+def check_acces(author, name=False):
+    is_owner = "#".join([author.name, author.discriminator])
+    if is_owner in operators:
+        return True
+    roles = author.roles
     for role in roles:
         if role.name == name or role.permissions.administrator:
                 return True
@@ -27,38 +33,54 @@ async def on_ready():
         #print(channel.name, dir(channel))
         if channel.name == prior_channel:
             await channel.send(greeting)
+    with open(log_output, "a") as log:
+        log.write("*"*10+"\nSTARTED\n")
     
 
 
 @client.event
 async def on_message(message):
+    print(message.author)
     channel = message.channel
-    author = message.author.name
-    author_id = author #ПЕРЕДЕЛАТЬ
+    sender = "#".join([message.author.name, message.author.discriminator])
     is_bot = message.author.bot
     msg = message.content
+    with open(log_output, "a") as log:
+        data = ["CHANNEL: "+str(channel), "SENDER: "+sender, "MSG: "+str(msg), "IsBot: ", str(is_bot)]
+        log.write("|".join(data)+"\n")
     
     #print("CHNL:", message.channel)
     print("MSG:",message)
     
     if not is_bot:
+        global m
+        m = message
         if msg.strip()[0] == prefix:
                 print("CMD")
                 await client.process_commands(message)
-
             
     
-@client.command()
+@client.command(brief='Отключение бота', description='Отключение бота (только для владельца и ограниченного круга доверенных лиц)')
 async def die(ctx):
     global d
     d=ctx
     author = ctx.author
-    if check_acces(author.roles, name="moderator"):
+    print("EMM")
+    if check_acces(author, name="moderator"):
         await ctx.send('Пока! :wave:')
         print("BYE")
         sys.exit(True)    
     else:
         await ctx.send('{}, ты ничего не перепутал?'.format(author.name))
+
+@client.command(brief=prefix+'hi [msg]', description='Отправка вашего сообщения от имени бота')
+async def hi(ctx, msg=None, *other):
+    if not msg:
+        msg = "И что я должен тебе сказать? И зачем?.."
+    else:
+        if other:
+            msg = msg + " " + " ".join(other)
+    await ctx.send("BOT: {}".format(msg))
 
 '''
 @client.command_not_found()
